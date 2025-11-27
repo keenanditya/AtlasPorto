a9947fd5da25393296d2acf9986d01dd592ccbb7
keenanditya <94579925+keenanditya@users.noreply.github.com>
2025-11-27 13:05:34 +0000

    capek

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,8 @@ import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import Home from "./pages/Home";
-import ProjectView from "@/pages/ProjectView";
-import NotFound from "@/pages/not-found";
+import ProjectView from "./pages/ProjectView";
+import NotFound from "./pages/not-found";
 
 function Router() {
   return (

11c91ce415dc7cf1aaec8aa98f5a3558f62ff3c2
keenanditya <94579925+keenanditya@users.noreply.github.com>
2025-11-27 12:57:24 +0000

    hapus tooltip

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/Home";
 import ProjectView from "@/pages/ProjectView";
 import NotFound from "@/pages/not-found";
@@ -19,9 +18,7 @@ function Router() {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
         <Router />
-      </TooltipProvider>
     </QueryClientProvider>
   );
 }
